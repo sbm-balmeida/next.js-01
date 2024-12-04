8a0c9647772359effe84263066de73841e614af4
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-12-04 17:57:02 -0300

    getServerSideProps

--- pages/faq.js
+++ pages/faq.js
@@ -1,54 +1,82 @@
 import { useEffect, useState } from 'react';
 import Link from '../src/components/Link';
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
 
+//     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    
+//     const faq = await fetch(FAQ_API_URL)
+//         .then((respostaDoServidor) => {
+//             return respostaDoServidor.json();
+//         })
+//         .then((resposta) => {
+//             setTimeout(() => {
+//                 setFaq(resposta);
+//             }, 3 * 1000);
+//         })
+//     ;
+
+//     return {
+//         props: {
+//             qualquercoisa: 'que eu passar aqui', faq
+//         },
+//     };
+// }
+
+export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     
     const faq = await fetch(FAQ_API_URL)
-        .then((respostaDoServidor) => {
-            return respostaDoServidor.json();
-        })
-        .then((resposta) => {
-            setTimeout(() => {
-                setFaq(resposta);
-            }, 3 * 1000);
-        })
-    ;
-
+      .then((res) => res.json());
+  
     return {
-        props: {
-            qualquercoisa: 'que eu passar aqui', faq
-        },
+      props: {
+        faq,
+      },
     };
-}
+  }
 
-export default function FAQPage(faq) {
-    console.log(faq)
-    // const [faq, setFaq] = useState([]);
+// export default function FAQPage({faq}) {
+//     return (
+//         <div>
+//             <h1>Alura Cases - Página de Perguntas FAQ</h1>
 
-    // useEffect(() => {
-        
-    // }, []);
+//             <Link href="/">
+//                 Ir para a Home
+//             </Link>
 
+//             <ul>
+//                 {faq.map(({ answer, question }) => (
+//                     <li key={question}>
+//                         <article>
+//                             <h2>{question}</h2>
+//                             <p>{answer}</p>
+//                         </article>
+//                     </li>
+//                 ))}
+//             </ul>
+//         </div>
+//     )
+// }
+
+
+export default function FAQPage({ faq }) {
     return (
-        <div>
-            <h1>Alura Cases - Página de Perguntas FAQ</h1>
-
-            <Link href="/">
-                Ir para a Home
-            </Link>
-
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+      <div>
+        <h1>Alura Cases - Página de Perguntas FAQ</h1>
+  
+        <Link href="/">Ir para a Home</Link>
+  
+        <ul>
+          {faq.map(({ answer, question }) => (
+            <li key={question}>
+              <article>
+                <h2>{question}</h2>
+                <p>{answer}</p>
+              </article>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
 }
