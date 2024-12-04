cf1593befc898f9c9cb3019465680c22cfc7df05
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-12-04 18:12:30 -0300

    Next e Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,20 @@ function MyApp({ Component, pageProps }) {
         <>
             <div>
                 <Head>
+                    {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2QZDPX3BLK"></script>
+                    <script 
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+
+                                gtag('config', 'G-2QZDPX3BLK');
+                            `
+                        }}
+                    />
+
                     <link rel="preconnect" href="https://fonts.googleapis.com"/>
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
                     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"/>
