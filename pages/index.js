656e75f8922c8b58cb384e1f60693cb867960171
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-12-04 13:33:33 -0300

    Onde colocar meu CSS?

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import { React } from 'react';
 import Link from '../src/components/Link';
+
+function Title({ children, as }) {
+    const Tag = as;
+
+    return (
+        <>
+            <Tag>
+                {children}
+            </Tag>
+
+            <style jsx>{`
+                ${Tag} {
+                    color: red;
+                    font-family: sans-serif;
+                }
+            `}</style>
+        </>
+    );
+}
 
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <Title as="h2">Alura Cases - Home Page</Title>
 
             <Link href="/faq">
                 Ir para a Home
